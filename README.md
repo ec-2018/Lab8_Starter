e72a9275a9df259e06a30f6a3b7bf20a7cd5f922
Elliot Chung <ec2018@gmail.com>
2021-11-14 22:58:07 -0800

    Finished lab

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Lab 8 - Starter
+1. Option 1. Testing can become a fairly repetitive action as development goes on as, ideally, you'd like to be confident that your code is passing all/most of your test with each iteration of your code. By using Github actions testing can be done automatically, saving time while maintaining the code's functionality.  
+2. No
+3. No, the messaging feature likely involves a lot of interacting systems to function. For example sending a message and receiving a message requires two very different bodies of code that need to be able to function together. The entire messaging feature cannot be broken down into a single 'unit' to test. 
+4. Yes probably, checking the length of an input is a self contained problem and it's unlikely to encroach on other areas of the application. 
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -1,5 +1,106 @@
 // unit.test.js
 
+const { exportAllDeclaration } = require('@babel/types');
+const { TestWatcher } = require('@jest/core');
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+test ('isPhoneNumber with valid 10 digit input', () => {
+    const number = '(619) 243-2935';
+    expect(functions.isPhoneNumber(number)).toBe(true);
+});
+
+test ('isPhoneNumber with no area code valid input', () => {
+    const number = '243-2935';
+    expect(functions.isPhoneNumber(number)).toBe(true);
+});
+
+test ('isPhoneNumber with 11 digit input', () => {
+    const number = '12345678901';
+    expect(functions.isPhoneNumber(number)).toBe(false);
+});
+
+test ('isPhoneNumber with letters', () => {
+    const number = 'abcdefghij';
+    expect(functions.isPhoneNumber(number)).toBe(false);
+});
+
+test ('isEmail with gmail.com', () => {
+    const email = 'loremipsum@gmail.com';
+    expect(functions.isEmail(email)).toBe(true);
+});
+
+test ('isEmail with international web domain', () => {
+    const email = 'loremipsum@mail.nz';
+    expect(functions.isEmail(email)).toBe(true);
+});
+
+test ('isEmail with no domain', () => {
+    const email = 'loremipsum';
+    expect(functions.isEmail(email)).toBe(false);
+});
+
+test ('isEmail with a space', () => {
+    const email = 'lorem ipsum@gmail.com';
+    expect(functions.isEmail(email)).toBe(false);
+});
+
+test ('isStrongPassword with a strong password', () => {
+    const password = 'YvhY2cbtHMVgkz';
+    expect(functions.isStrongPassword(password)).toBe(true);
+});
+
+test ('isStrongPassword with only letters', () => {
+    const password = 'YvhYacbtHMVgkz';
+    expect(functions.isStrongPassword(password)).toBe(true);
+});
+
+test ('isStrongPassword with long input', () => {
+    const password = 'YvhY2cbtHMVgasdfkkzx';
+    expect(functions.isStrongPassword(password)).toBe(false);
+});
+
+test ('isStrongPassword with only numbers', () => {
+    const password = '1523452534';
+    expect(functions.isStrongPassword(password)).toBe(false);
+});
+
+test ('isDate with valid input', () => {
+    const date = '12/12/2021';
+    expect(functions.isDate(date)).toBe(true);
+});
+
+test ('isDate with single digit month and day', () => {
+    const date = '1/1/2021';
+    expect(functions.isDate(date)).toBe(true);
+});
+
+test ('isDate without a year', () => {
+    const date = '01/01';
+    expect(functions.isDate(date)).toBe(false);
+});
+
+test ('isDate 8000 years in the future', () => {
+    const date = '01/01/10021';
+    expect(functions.isDate(date)).toBe(false);
+});
+
+test ('isHexColor with valid 6 digit hex color', () => {
+    const color = '4287f5';
+    expect(functions.isHexColor(color)).toBe(true);
+});
+
+test ('isHexColor with valid 3 digit hex color', () => {
+    const color = '7f5';
+    expect(functions.isHexColor(color)).toBe(true);
+});
+
+test ('isHexColor with too many digits', () => {
+    const color = '4287f5b';
+    expect(functions.isHexColor(color)).toBe(false);
+});
+
+test ('isHexColor with too large a value', () => {
+    const color = '4287fj';
+    expect(functions.isHexColor(color)).toBe(false);
+});
